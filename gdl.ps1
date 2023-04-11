d0570989e7cf3c81e9f1c236a99f30d07d64a75d
Quentin-m2i <125981584+Quentin-m2i@users.noreply.github.com>
2023-04-11 13:44:41 +0200

    Create gdl.ps1

---
+++ gdl.ps1
@@ -0,0 +1,41 @@
+# Variables
+
+$fichier='Communs','Compta','Recherches','Informatiques','Ventes','Clients','Paies','Plans' # Les fichier que nous souhaitons couvrir
+$droit='CT','M','RW','RX','RO'
+
+# Le domaine et le TLD en arguments
+$DC1=$args[0] 
+$DC2=$args[1] 
+
+# Compteur pour les tableaux fichier et droits
+$fi=0
+$fi2=0
+
+# Code
+
+
+import-module activedirectory
+
+# On crée une OU pour ranger nos GDL avec les droits
+write-host "Creation de l'OU_Sharefile"
+New-ADOrganizationalUnit -Name "OU_ShareFile" -Path "DC=$DC1,DC=$DC2"
+
+
+
+while($fi -lt 7)
+{
+  # Mise en place du noms du GDL
+	$fichiermaintenant=$fichier[$fi]
+	$droitmaintenant=$droit[$fi2]
+	$nomgdl="GDL_${fichiermaintenant}_${droitmaintenant}"
+  
+  # Créations du GDL
+	write-host "Création de :$nomgdl"
+	New-ADGroup -Name "$nomgdl" -GroupScope DomainLocal -groupcategory Security  -Path "OU=OU_ShareFile,DC=$DC1,DC=$DC2"
+	$fi2=$fi2+1
+	if ($fi2 -gt 4)
+	{
+		$fi2=0
+		$fi=$fi+1
+	}
+}
